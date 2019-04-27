ca7c03a4b6adddd449d1adbe2f61a77650a38330
thiagofuda <thifuda@gmail.com>
2019-04-27 20:20:47 -0300

    Update telnet.sh

--- thiagofuda/telnet.sh
+++ thiagofuda/telnet.sh
@@ -2,9 +2,9 @@
 porta1=80
 porta2=443
 echo "d3-hangry-stg1 $porta1
-a1-hostname-1 $porta2
-a1-hostname-2 $porta1
-a1-hostname-3 $porta2" | \
+hostname-1 $porta2
+ahostname-2 $porta1
+hostname-3 $porta2" | \
 while read host port; do
   r=$(bash -c 'exec 3<> /dev/tcp/'$host'/'$port';echo $?' 2>/dev/null)
   if [ "$r" = "0" ]; then
